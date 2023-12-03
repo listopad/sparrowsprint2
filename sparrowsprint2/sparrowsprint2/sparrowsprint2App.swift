b84b66438fd04b25f42d002820d9537ee0c73fd0
Artem <adelelain@gmail.com>
2023-12-03 15:23:32 +0300

    New project

---
+++ sparrowsprint2/sparrowsprint2/sparrowsprint2App.swift
@@ -0,0 +1,17 @@
+//
+//  sparrowsprint2App.swift
+//  sparrowsprint2
+//
+//  Created by Artem Dragunov on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct sparrowsprint2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sparrowsprint2/sparrowsprint2UITests/sparrowsprint2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sparrowsprint2UITestsLaunchTests.swift
+//  sparrowsprint2UITests
+//
+//  Created by Artem Dragunov on 03.12.2023.
+//
+
+import XCTest
+
+final class sparrowsprint2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
